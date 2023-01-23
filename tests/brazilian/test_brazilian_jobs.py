ca9173855c37e4aae00fbe50ecab6bfaa25967e6
GiovannaMorais <gigimorais9@hotmail.com>
2023-01-23 20:43:25 -0300

    Feat: Implement a test for the read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+mock = {"title": "Pessoa Programadora", "salary": "3000", "type": "full time"}
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+
+    for job in brazilian_jobs:
+        assert mock.keys() == job.keys()
