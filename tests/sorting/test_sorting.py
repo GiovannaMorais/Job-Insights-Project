1e1f205e6af75052dfbd92412921aee86676c019
GiovannaMorais <gigimorais9@hotmail.com>
2023-01-23 20:43:45 -0300

    Feat: Implement a test for the sort_by function

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,28 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+
+mock = [
+    {"min_salary": 1000, "max_salary": 3000},
+    {"min_salary": 4000, "max_salary": 8000},
+    {"min_salary": 10000, "max_salary": 16000},
+]
+
+order_min_salary = [
+    {"min_salary": 1000, "max_salary": 3000},
+    {"min_salary": 4000, "max_salary": 8000},
+    {"min_salary": 10000, "max_salary": 16000},
+]
+
+order_max_salary = [
+    {"min_salary": 10000, "max_salary": 16000},
+    {"min_salary": 4000, "max_salary": 8000},
+    {"min_salary": 1000, "max_salary": 3000},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(mock, "min_salary")
+    assert mock == order_min_salary
+
+    sort_by(mock, "max_salary")
+    assert mock == order_max_salary
